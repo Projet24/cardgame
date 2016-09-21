57acdbcfafa576b6cc955374b281f8c7742b964d
vincent-heng <hengvincent.1994@gmail.com>
2016-09-21 19:15:34 +0200

    //TODO replace Dynamic types with the right types

--- com/p24studio/cardgame/main/service/Database.hx
+++ com/p24studio/cardgame/main/service/Database.hx
@@ -1,11 +1,17 @@
 package com.p24studio.cardgame.main.service;
 
+/*@:jsRequire("sqlite3", "Statement")
+extern class Statement {
+  function run(?params:List<Dynamic>):Void;
+  function finalize():Void;
+}*/
+
 @:jsRequire("sqlite3", "Database")
-extern class Database { //TODO replace Dynamic types with the right types
+extern class Database {
     function new(file:String);
-    function serialize(request:Dynamic):Dynamic;
-    function run(request:Dynamic):Database;
-    function prepare(request:Dynamic):Dynamic;
-    function each(statement:Dynamic, ?params: Dynamic, ?callback:Dynamic -> Dynamic -> Void, ?complete:Dynamic):Database;
-    function all(statement:Dynamic, ?params: Dynamic, ?callback:Dynamic -> Array<Dynamic> -> Void):Database;
+    function serialize(request:Void -> Void):Database;
+    function run(request:Dynamic, ?params:List<String>):Database;
+    function prepare(request:String):Dynamic;
+    function each(statement:Dynamic, ?params:Dynamic, ?callback:Dynamic -> Dynamic -> Void, ?complete:Dynamic):Database;
+    function all(statement:Dynamic, ?params:Dynamic, ?callback:Dynamic -> Array<Dynamic> -> Void):Database;
 }
